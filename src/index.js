b7ecd51d695dca3a21fffc1748a6b03550fd103a
Rui-Sun <sunrui1992@foxmail.com>
2018-05-06 21:53:04 +0800

    完成动画部分

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ rule.init();
 
 function flash() {
     for(let i = 0; i < 16; i++) {
-        const text = Math.pow(2, rule._box[`${Math.floor(i / 4)}${Math.floor(i % 4)}`]);
-        document.getElementById(`con-${i + 1}`).getElementsByClassName('number-text')[0].innerHTML = text !== 1 ? text : '';
+        const posiRow = Math.floor(i / 4);
+        const posiCol = Math.floor(i % 4);
+        const text = Math.pow(2, rule._box[`${posiRow}${posiCol}`]);
+
+        const dom = document.getElementById(`con-${i + 1}`);
+        dom.className = dom.className.replace(' number-container-transition', '');
+        dom.style.top = posiRow * 125 + 'px';
+        dom.style.left = posiCol * 125 + 'px';
+
+        dom.getElementsByClassName('number-text')[0].innerHTML = text !== 1 ? text : '';
         document.getElementById('score').innerHTML = rule.getScore();
     }
 }
@@ -17,22 +25,37 @@ flash();
 document.addEventListener('keydown', (event) => {
     switch (event.keyCode) {
         case 38:
-            rule.move(0);
-            flash()
+            move(0);
             break;
         case 37:
-            rule.move(1);
-            flash()
+            move(1);
             break;
         case 39:
-            rule.move(3);
-            flash()
+            move(3);
             break;
         case 40:
-            rule.move(2);
-            flash()
+            move(2);
             break;
         default:
             break;
     }
 });
+
+function move(dir) {
+    const result = rule.move(dir);
+    result.posiChange.forEach((item) => {
+        const oldPosi = item[0].split('');
+        const oldIndex = Number(oldPosi[0]) * 4 + Number(oldPosi[1]) + 1;
+        const newPosi = item[1].split('');
+        const dom = document.getElementById(`con-${oldIndex}`)
+        dom.className = dom.className + ' number-container-transition';
+        dom.style.top = newPosi[0] * 125 + 'px';
+        dom.style.left = newPosi[1] * 125 + 'px';
+    });
+    setTimeout(() => {
+        flash();
+        if (result.gameEndFlag) {
+            alert('游戏结束！');
+        }
+    }, 210);
+}
--- src/rule.js
+++ src/rule.js
@@ -1,13 +1,11 @@
 // 将计数简化为1-11
 class Rule {
-    constructor(endCallback) {
-        // 结束时的回调函数，传入移动步数
-        if (endCallback) this._endCallback = endCallback;
+    constructor() {
         // flag:是否在游戏中，
         this._isPlaying = false;
         // flag:是否在动画中，在动画中停止交互
         this._isAnimating = false;
-        // 总步数
+        // 总分数
         this._score = 0;
         // 数字池，使用key-value结构
         this._box = {};
@@ -46,44 +44,61 @@ class Rule {
     // 移动，0123对应方向上左下右
     move(dir) {
         // 游戏结束或正在动画中不移动
+        // debugger
         if (!this._isPlaying || this._isAnimating) return;
 
         let gameEndFlag = false;
+        let isMoving = false;
+        const posiChange = [];
 
         for (let i = 0; i < 4; i++) {
             const tempArr = [];
             const resultArr = [];
             for (let j = 0; j < 4; j++) {
                 if (this._getNum(this._getKey(dir, i, j)) !== 0) {
-                    tempArr.push(this._getNum(this._getKey(dir, i, j)));
+                    const key = this._getKey(dir, i, j);
+                    tempArr.push({
+                        // 记录每一个不为0的值的老位置
+                        key: key,
+                        num: this._getNum(key)
+                    });
                 }
             }
 
             for (let k = 0; k < tempArr.length; k++) {
-                if (tempArr[k] === 0) {
-                    continue;
-                } else if (tempArr[k + 1] && tempArr[k] === tempArr[k + 1]) {
-                    tempArr[k]++;
-                    this._score += Math.pow(2, tempArr[k]);
-                    tempArr[k + 1] = 0;
-                    resultArr.push(tempArr[k]);
-                } else {
-                    resultArr.push(tempArr[k]);
+                if (tempArr[k + 1] && tempArr[k].num === tempArr[k + 1].num) {
+                    tempArr[k].num++;
+                    this._score += Math.pow(2, tempArr[k].num);
+                    tempArr[k + 1].num = 0;
                 }
+                isMoving = true;
             }
 
             for (let m = 0; m < 4; m++) {
-                if (resultArr[m]) {
-                    this._setNum(this._getKey(dir, i, m), resultArr[m]);
-                    if (resultArr[m] === 11) gameEndFlag = true;
+                if (tempArr[m] && tempArr[m].num !== 0) {
+                    const key = this._getKey(dir, i, m);
+                    this._setNum(key, tempArr[m].num);
+                    if (tempArr[m].num === 11) gameEndFlag = true;
+
+                    // 记录新位置
+                    tempArr[m].newKey = key
                 } else {
                     this._setNum(this._getKey(dir, i, m), 0);
                 }
             }
+
+            for (let n = 0; n < tempArr.length; n++) {
+                if (tempArr[n].num === 0) tempArr[n].newKey = tempArr[n - 1].newKey;
+                posiChange.push([tempArr[n].key, tempArr[n].newKey]);
+            }
         }
 
-        this._setNum(this._getRandomPosi(), this._get1Or2());
+        if (isMoving) this._setNum(this._getRandomPosi(), this._get1Or2());
         if (gameEndFlag) this._end();
+        return {
+            posiChange,
+            isEnd: !this._isPlaying
+        }
     }
 
     // 设置动画状态
@@ -99,8 +114,6 @@ class Rule {
     // 游戏结束
     _end() {
         this._isPlaying = false;
-        this._endCallback(this._score);
-        return this;
     }
 
     // 获取数字
